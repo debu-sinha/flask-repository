3eed0099cdee3b64f37a5e8dc7872b77305dae3b
dsinha3 <debusinha2009@gmail.com>
2020-08-26 12:51:52 -0400

    updated requirements.txt

--- app.py
+++ app.py
@@ -32,4 +32,5 @@ if __name__ == '__main__':
     from db import db
     db.init_app(app)
     #reset_database()
-    app.run(port = 5000, debug = True)
+    #app.run(port = 5000, debug = True)
+    app.run()
